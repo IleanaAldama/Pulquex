5bfaa072ee1e5c54dfe07bf0ad84c7449b817a4a
ileana aldama <ileana.aldama@payjoy.com>
2026-04-14 15:34:38 -0600

    refactor genserver

--- lib/pulquex/storage.ex
+++ lib/pulquex/storage.ex
@@ -8,12 +8,59 @@ defmodule Pulquex.Storage do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  @impl true
   def init(_opts) do
-    :ets.new(@table_name, [:set, :public, :named_table, read_concurrency: true])
+    :ets.new(@table_name, [:set, :private, :named_table, read_concurrency: true])
+    schedule_cleanup()
     {:ok, %{}}
   end
 
   def insert(challenge = %Challenge{}, ttl \\ 100) do
+    GenServer.cast(__MODULE__, {:insert, challenge, ttl})
+    :ok
+  end
+
+  def get(id) do
+    GenServer.call(__MODULE__, {:get, id})
+  end
+
+  def mark_as_used(id) do
+    GenServer.cast(__MODULE__, {:mark_as_used, id})
+    :ok
+  end
+
+  @impl true
+  def handle_info(:cleanup, state) do
+    now = DateTime.utc_now() |> DateTime.to_unix(:second)
+    :ets.select_delete(@table_name, [{{:_, :_, :"$1", :_}, [{:<, :"$1", now}], [true]}])
+    schedule_cleanup()
+    {:noreply, state}
+  end
+
+  @impl true
+  def handle_call({:get, id}, _from, state) do
+    now = DateTime.utc_now() |> DateTime.to_unix(:second)
+
+    result =
+      case :ets.lookup(@table_name, id) do
+        [] ->
+          {:error, :not_found}
+
+        [{^id, _challenge, expires_at, _used?}] when now > expires_at ->
+          {:error, :expired}
+
+        [{^id, _challenge, _expires_at, _used? = true}] ->
+          {:error, :used}
+
+        [{^id, challenge, _expires_at, _used?}] ->
+          {:ok, challenge}
+      end
+
+    {:reply, result, state}
+  end
+
+  @impl true
+  def handle_cast({:insert, challenge, ttl}, state) do
     expires_at = (DateTime.utc_now() |> DateTime.to_unix(:second)) + ttl
 
     :ets.insert(
@@ -21,29 +68,13 @@ defmodule Pulquex.Storage do
       {challenge.id, challenge, expires_at, false}
     )
 
-    :ok
+    {:noreply, state}
   end
 
-  def get(id) do
-    now = DateTime.utc_now() |> DateTime.to_unix(:second)
-
-    case :ets.lookup(@table_name, id) do
-      [] ->
-        {:error, :not_found}
-
-      [{^id, _challenge, expires_at, _used?}] when now > expires_at ->
-        {:error, :expired}
-
-      [{^id, _challenge, _expires_at, _used? = true}] ->
-        {:error, :used}
-
-      [{^id, challenge, _expires_at, _used?}] ->
-        {:ok, challenge}
-    end
-  end
-
-  def mark_as_used(id) do
+  def handle_cast({:mark_as_used, id}, state) do
     :ets.update_element(@table_name, id, {4, true})
-    :ok
+    {:noreply, state}
   end
+
+  defp schedule_cleanup, do: Process.send_after(self(), :cleanup, :timer.minutes(5))
 end
